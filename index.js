123146e8aa76730a96fcc0cddd22565ceca65b12
john karl <johnkarl.limjoco@gmail.com>
2024-11-07 09:56:21 +0800

    make dynamic video Title

--- index.js
+++ index.js
@@ -5,21 +5,27 @@ function getQueryParam(param) {
 
 document.addEventListener("DOMContentLoaded", function () {
   const videoSrc = getQueryParam("video");
-
-
+  const dynamicTitle = document.getElementById("dynamicTitle");
+  if (videoSrc.includes("math")) {
+    dynamicTitle.textContent = "Math Class";
+  } else if (videoSrc.includes("drawing")) {
+    dynamicTitle.textContent = "Drawing Class";
+  }
 
   if (videoSrc) {
+    let title = videoSrc.charAt(0).toUpperCase() + videoSrc.slice(1) + " Class"; // Capitalize first letter
+
+    // Set the dynamic title to the h1 element
+
     const videoElement = document.querySelector("video");
     const sourceElement = document.getElementById("videoSource");
     sourceElement.src = videoSrc;
-    videoElement.load(); 
-
-
+    videoElement.load();
 
     videoElement.play().catch((error) => {
       console.error("Error playing video:", error);
     });
   } else {
-    console.log("No video source found."); 
+    console.log("No video source found.");
   }
 });
